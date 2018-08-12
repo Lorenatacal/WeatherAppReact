f4a93a3984738ffba6618b3dce99dcbf22ecc83d
Lorenatacal <Lorena.tacal@gmail.com>
2018-08-12 22:56:20 +0100

    Add the header of the project Local Weather

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src= "https://img.clipartxtras.com/96da9eda759c08834fa44d63ab4098dd_weather-png-images-transparent-free-download-pngmartcom-weather-clipart-png_512-512.png" className="App-logo" alt="logo" />
+          <h1 className="App-title">Local Weather</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
