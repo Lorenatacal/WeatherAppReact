fda17f8bc0513f18d12c6d92bcd2ca9547364d24
Lorenatacal <Lorena.tacal@gmail.com>
2018-08-14 09:50:36 +0100

    Add the react component that is showing the location of the user

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 import Header from './Header';
-
+import GetLocation from './GetLocation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <GetLocation />
       </div>
     );
   }
