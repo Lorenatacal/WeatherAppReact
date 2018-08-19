1d129be9c669dc40a369a381feb0f4e328465f6d
Lorenatacal <Lorena.tacal@gmail.com>
2018-08-19 22:43:50 +0100

    Render the location from the API data and stop an infinite loop

--- src/Weather.js
+++ src/Weather.js
@@ -5,10 +5,20 @@ import { geolocated } from 'react-geolocated';
 class Weather extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            response: false,
+            name: '',
+            country: '',
+            description: '',
+            icon: '',
+            temperature: 0,
+            wind: 0,
+            humidity:0,
+        }
     }
 
     render() {
-        if (this.props.coords){
+        if (this.props.coords && !this.state.response){
             axios.get('https://api.openweathermap.org/data/2.5/weather?', {
                 params: {
                     lat: this.props.coords.latitude,
@@ -16,18 +26,22 @@ class Weather extends React.Component {
                     appid: process.env.REACT_APP_WEATHER_API_KEY,
                 }
             })
-            .then(function (response) {
+            .then((response) => {
                 console.log(response)
+                this.setState({
+                    response: true,
+                    name: response.data.name,
+                })
             })
         }
         return(  
             <div>
-                <h2> The weather today in:</h2> 
-                <h3> Today is going to be (main weather)  (main description)</h3>
-                <h3> (main icon) </h3>
-                <h3> The temperature is: </h3>
-                <h3> The wind is blowing with: </h3>
-                <h3> and we have a humidity of: </h3>
+                <h2> The weather today in: {this.state.name}, {this.state.country}</h2> 
+                <h3> Today is going to be {this.state.description}</h3>
+                <h3> {this.state.icon} </h3>
+                <h3> The temperature is: {this.state.temperature} </h3>
+                <h3> The wind is blowing with: {this.state.wind} </h3>
+                <h3> and we have a humidity of: {this.state.humidity}</h3>
             </div>
         )
     }
